e0a18134d0d29c0e08178f127bffe2715ffad9fb
Kaylee Swenson <kheides@gmail.com>
2023-04-08 11:36:32 -0500

    idk

--- GoodDaySafetyApp/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/GoodDaySafetyApp.AssemblyInfo.cs
+++ GoodDaySafetyApp/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/GoodDaySafetyApp.AssemblyInfo.cs
@@ -11,14 +11,10 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PackageName", "3311b97f-2f49-434a-bb49-68882975e9a6")]
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PublisherName", "User Name")]
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Name", "GoodDaySafetyApp")]
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Version", "1.0.0.1")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("GoodDaySafetyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GoodDaySafetyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GoodDaySafetyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
