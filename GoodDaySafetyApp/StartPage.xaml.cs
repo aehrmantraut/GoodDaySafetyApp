d6a9ef9484bb70e6c701f36b74b7844ec504011a
beskro2 <ben.eskro@ndus.edu>
2023-04-21 14:44:52 -0500

    database connection

--- GoodDaySafetyApp/StartPage.xaml.cs
+++ GoodDaySafetyApp/StartPage.xaml.cs
@@ -1,14 +1,24 @@
+
+
+
 namespace GoodDaySafetyApp;
 
 public partial class StartPage : ContentPage
 {
+    public string name;
 	public StartPage()
 	{
 		InitializeComponent();
 	}
+    
 
     private void NextPage_Click(object sender, EventArgs e)
     {
+         name = entry.Text;
+        
         Navigation.PushAsync(new SceneSafe());
+      
     }
+    
+  
 }
